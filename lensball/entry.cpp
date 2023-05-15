efea7e4d08413620118f281f4a506fd67fd78c86
遼太郎 梅本 <lissyno_original@outlook.jp>
2023-05-15 12:23:34 +0900

    複数レイを処理できるように

--- lensball/entry.cpp
+++ lensball/entry.cpp
@@ -19,31 +19,32 @@ int main() {
 	//レイを生成する
 	list<ray3> rays;
 	rays.push_back(ray3(arrow3(uvec3(0.25,0.,10.),uvec3(0.,0.,-1.))));//初期位置を作る
-	ray3& target = rays.back();
+	rays.push_back(ray3(arrow3(uvec3(-0.25, 0., 10.), uvec3(0., 0., -1.))));//初期位置を作る
+	for (ray3& target : rays) {
+		//レイトレパイプライン
+		[&] {
+			const auto rez0 = IntersectSphere(target.back(), nodeLensParam.first, nodeLensParam.second);//要素レンズと交差
 
-	//レイトレパイプライン
-	[&] {
-		const auto rez0 = IntersectSphere(target.back(), nodeLensParam.first, nodeLensParam.second);//要素レンズと交差
+			//当たらなかったらこれ以上やる必要はない
+			if (!rez0.isHit) {
+				FreeFlightRay(target);
+				return;
+			}
 
-		//当たらなかったらこれ以上やる必要はない
-		if (!rez0.isHit) {
+			//ここからレンズ内の処理
+			rez0.ApplyToRay(target);
+			if (!RefractSnell(target, rez0.norm, nodeLensEta))throw runtime_error("全反射が起きた");//屈折計算
+
+			const auto rez1 = IntersectSphere(target.back(), nodeLensParam.first, nodeLensParam.second);//要素レンズ内部を通過
+			if (!rez1.isHit)throw logic_error("logic err0");//レンズ内部なので絶対当たる
+			rez1.ApplyToRay(target);//進める
+
+			if (!RefractSnell(target, -rez1.norm, 1. / nodeLensEta))throw runtime_error("全反射が起きた");//屈折計算
+
 			FreeFlightRay(target);
-			return;
-		}
 
-		//ここからレンズ内の処理
-		rez0.ApplyToRay(target);
-		if(!RefractSnell(target, rez0.norm, nodeLensEta))throw runtime_error("全反射が起きた");//屈折計算
-		
-		const auto rez1 = IntersectSphere(target.back(), nodeLensParam.first, nodeLensParam.second);//要素レンズ内部を通過
-		if (!rez1.isHit)throw logic_error("logic err0");//レンズ内部なので絶対当たる
-		rez1.ApplyToRay(target);//進める
-
-		if (!RefractSnell(target, -rez1.norm, 1./nodeLensEta))throw runtime_error("全反射が起きた");//屈折計算
-
-		FreeFlightRay(target);
-
-	}();
+		}();
+	}
 
 	//要素レンズを描画
 	DrawSphere(plotter, nodeLensParam.first, nodeLensParam.second, 10, R"("red")");
