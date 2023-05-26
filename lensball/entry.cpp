5825bce8d8a4c23dbe34848da2363bfdbc0bac93
遼太郎 梅本 <lissyno_original@outlook.jp>
2023-05-26 15:35:37 +0900

    レイをいい感じに生成 ノードレンズもいい感じ

--- lensball/entry.cpp
+++ lensball/entry.cpp
@@ -19,31 +19,51 @@ int main() {
 
 	auto plotter = SetupPythonRuntime();//pythonをセットアップする
 	DefinePythonFunctions(plotter);//梅本的基本関数を定義
+	;
 
-	//要素レンズを定義　旧レンズに
-	const ureal nodeLensEta = 1.5;
-	sphereParam nodeLensParam = make_pair(uvec3(100.,0., 0.), 1.);
 
+	//レンズを定義していく
+	constexpr ureal nodeLensEta = 1.5;
+	constexpr ureal lensBallRadius = 5. / 2.;
+	constexpr unsigned int nodeLensNum = 10;
+	const ureal nodeLensRadius = lensBallRadius * std::sin((std::numbers::pi /(nodeLensNum-1.))/2.);//内接する2(n-1)角形の一辺の長さが直径だから...
+	std::list<sphereParam> nodeLensesParams;
+	for (std::decay<decltype(nodeLensNum)>::type i = 0; i < nodeLensNum; i++) {
+		//値を正規化する-1~1
+		const ureal reg = 2. * (i / (ureal)(nodeLensNum - 1)) - 1.;
+		//それを角度にする
+		const ureal angle = reg * std::numbers::pi/2.;//鉛直からの角度
+
+		//つまり位置が決まる x,y平面でy軸方向をzeroにangleだけ角度を決める
+		const uvec3 pos(lensBallRadius * sin(angle), lensBallRadius * cos(angle), 0.);
+		nodeLensesParams.push_back(sphereParam(pos, nodeLensRadius));
+	}
 	//レイを生成する
 	list<ray3> rays;
-	constexpr unsigned int raynum = 18;
-	constexpr ureal projectionLimit = 30. / 180. * std::numbers::pi;//片側角度
+	constexpr unsigned int targetBallsNum = 4;//光を当てるボールの数v ただしnodelensNumが偶数だったら偶数、奇数だったら奇数にすること
+	constexpr unsigned int targetBallsOffset = ((nodeLensNum % 2) != (targetBallsNum % 2)) ? 0 : (nodeLensNum - targetBallsNum) / 2;
 	const uvec3 projectionOrign(0., 0., 0.);
-	for (std::decay<decltype(raynum)>::type i = 0; i < raynum; i++) {
-		//値を正規化する-1~1
-		const ureal reg = 2. * (i / (ureal)(raynum - 1)) - 1.;
-		//それを角度にする
-		const ureal angle = reg * projectionLimit;//鉛直からの角度
+	constexpr ureal layAngleLimitFromTarget = 5. / 180. * std::numbers::pi;
+	auto ite=nodeLensesParams.begin();
+	for (int i = 0; i < targetBallsOffset; i++)ite++;
+	for (std::decay<decltype(targetBallsNum)>::type i = 0; i < targetBallsNum; i++) {
+		const uvec3 targetDirection = ((ite->first) - projectionOrign).normalized();//これがレンズの中心を通るレイの方向
+		//ここから+-thetaだけz軸中心に打ち分けたい
+		const uvec3 direction0 = Eigen::AngleAxis<ureal>(layAngleLimitFromTarget, uvec3(0, 0, 1)) * targetDirection;
+		const uvec3 direction1 = Eigen::AngleAxis<ureal>(-layAngleLimitFromTarget, uvec3(0, 0, 1))*targetDirection;
 
-		//つまり向きが決まる x,y平面でy軸方向をzeroにangleだけ角度を決める
-		const uvec3 direction(sin(angle), cos(angle), 0.);
-		rays.push_back(ray3(arrow3(projectionOrign, direction)));//レイを作成
+
+		rays.push_back(ray3(arrow3(projectionOrign, targetDirection)));//レイを作成
+		rays.push_back(ray3(arrow3(projectionOrign, direction0)));//レイを作成
+		rays.push_back(ray3(arrow3(projectionOrign, direction1)));//レイを作成
+
+		ite++;
 	}
 	for (ray3& target : rays) {
 		//レイトレパイプライン
 		[&] {
 			try {
-				const auto rez0 = IntersectSphere(target.back(), nodeLensParam.first, nodeLensParam.second);//要素レンズと交差
+				const auto rez0 = IntersectSphere(target.back(), nodeLensesParams.front().first, nodeLensesParams.front().second);//要素レンズと交差
 
 				//当たらなかったらこれ以上やる必要はない
 				if (!rez0.isHit) {
@@ -55,7 +75,7 @@ int main() {
 				rez0.ApplyToRay(target);
 				if (!RefractSnell(target, rez0.norm, nodeLensEta))throw runtime_error("全反射が起きた");//屈折計算
 
-				const auto rez1 = IntersectSphere(target.back(), nodeLensParam.first, nodeLensParam.second);//要素レンズ内部を通過
+				const auto rez1 = IntersectSphere(target.back(), nodeLensesParams.front().first, nodeLensesParams.front().second);//要素レンズ内部を通過
 				if (!rez1.isHit)throw logic_error("logic err0");//レンズ内部なので絶対当たる
 				rez1.ApplyToRay(target);//進める
 
@@ -76,7 +96,8 @@ int main() {
 	}
 
 	//要素レンズを描画
-	//DrawSphere(plotter, nodeLensParam.first, nodeLensParam.second, 10, R"("red")");
+	for (const auto& i : nodeLensesParams)
+		DrawSphere(plotter, i.first, i.second, 20, R"("green")");
 	//すべてのレイを描画
 	const array<string,5> cols = { "\"red\"","\"orange\"","\"yellow\"","\"green\"","\"blue\"" };
 	for (const auto& r : rays) {
