ab2eac39cee1f62e5fe73c62fe4d3b9a6f1e237c
遼太郎 梅本 <lissyno_original@outlook.jp>
2023-05-31 13:51:18 +0900

    もうちょい

--- lensball/entry.cpp
+++ lensball/entry.cpp
@@ -6,11 +6,8 @@
 using namespace std;
 
 constexpr ureal theta = 30. / 180. * std::numbers::pi;//こんだけ傾ける
-constexpr ureal scanHeight = 0.0;
+const std::pair<ureal,ureal> scanHeightRange = make_pair(-0.5,0.5);
 
-uvec3 ScanPointGlobal(const ureal t) {
-	return uvec3((cos(t + theta) + cos(t - theta)) / 2., sin(t), (sin(theta + t) + sin(theta - t))/2.);
-}
 //いろんなスキャンポイント
 uvec2 ScanPointWithGh(ureal t,ureal gh,ureal theta) {
 	const ureal radius = sqrt(1. - pow(gh, 2));//スキャン高さでの軌跡の半径
@@ -24,26 +21,34 @@ int main() {
 		//plotの準備
 		auto plotter = SetupPythonRuntime();//pythonをセットアップする
 		DefinePythonFunctions(plotter);//梅本的基本関数を定義
-		plotter->send_command("t=[]\nv=[]\nr=[]\n");
 
-		constexpr size_t circleRes = 360;
-		for (std::decay<decltype(circleRes)>::type phi = 0; phi < circleRes; phi++) {
-			const ureal t = uleap(std::make_pair(-std::numbers::pi, +std::numbers::pi), phi / (ureal)circleRes);//tを計算
+		//高さを変えてplt
+		constexpr size_t scanheightResolution = 10;
+		for (std::decay<decltype(scanheightResolution)>::type h = 0; h < scanheightResolution; h++) {
+			plotter->send_command("plt.clf()\n");
+			plotter->send_command("t=[]\nv=[]\nr=[]\n");
 
-			const auto scanP = ScanPointWithGh(t, scanHeight, theta);//UV座標ゲット
-			
-			//これをuv座標に
-			const ureal v = scanP.y();
-			const ureal r = scanP.x();
+			//いまのscan高さ
+			const auto scanHeight = uleap(scanHeightRange, h / (ureal)(scanheightResolution - 1));
+			cout << scanHeight << endl;
 
-			plotter->send_command(StringFormat("t.append(%f)\nv.append(%f)\nr.append(%f)\n", t, v, r));
+			constexpr size_t circleRes = 360;
+			for (std::decay<decltype(circleRes)>::type phi = 0; phi < circleRes; phi++) {
+				const ureal t = uleap(std::make_pair(-std::numbers::pi, +std::numbers::pi), phi / (ureal)circleRes);//tを計算
+
+				const auto scanUV = ScanPointWithGh(t, scanHeight, theta);//UV座標ゲット UV球ローカル
+
+				plotter->send_command(StringFormat("t.append(%f)\nv.append(%f)\nr.append(%f)\n", t, scanUV.y(), scanUV.x()));
+			}
+
+			plotter->send_command(StringFormat("plt.text(0,-1.5,\"theta = 30[deg], scan height = %f\")",scanHeight));
+			plotter->send_command("plt.plot(t,r,label=\"r [rad]\",color=\"magenta\")\n");
+			plotter->send_command("plt.plot(t,v,label=\"v\",color=\"cyan\")\n");
+			plotter->send_command("plt.xlabel(\"Rotation angle [rad]\")\nplt.ylabel(\"UV coordition\")\n");
+			plotter->send_command("plt.legend()\n");
+			plotter->pause(.1);
+			plotter->save(StringFormat("C:/local/user/lensball/lensball/results3/rez%d.png",h));
 		}
-
-		plotter->send_command("plt.clf()\nplt.plot(t,r,label=\"r [rad]\",color=\"magenta\")\n");
-		plotter->send_command("plt.plot(t,v,label=\"v\",color=\"cyan\")\n");
-		plotter->send_command("plt.xlabel(\"Rotation angle [rad]\")\nplt.ylabel(\"UV coordition\")\n");
-		plotter->send_command("plt.legend()\n");
-		plotter->show();
 	}
 	catch (std::exception& ex) {
 		cout << ex.what() << endl;
