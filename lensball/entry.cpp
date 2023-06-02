ff8a180311396da31b7b527c56f523e0e6b378ca
遼太郎 梅本 <lissyno_original@outlook.jp>
2023-06-02 18:57:51 +0900

    evenにスキャンできるようにレンズパスの概念を導入

--- lensball/entry.cpp
+++ lensball/entry.cpp
@@ -6,13 +6,25 @@
 using namespace std;
 
 constexpr ureal theta = 30. / 180. * std::numbers::pi;//こんだけ傾ける
-const std::pair<ureal,ureal> scanHeightRange = make_pair(-0.5,0.5);
+const std::pair<ureal,ureal> scanHeightRange = make_pair(-0.8,0.8);
 
-//いろんなスキャンポイント
+//ローカルから見たスキャン位置　t 回転角度,gh スキャンラインの高さ(グローバル)　theta ローカルの傾き
 uvec2 ScanPointWithGh(ureal t,ureal gh,ureal theta) {
 	const ureal radius = sqrt(1. - pow(gh, 2));//スキャン高さでの軌跡の半径
 	//r,v
 	return uvec2(atan((radius * sin(t)) / (radius * cos(t) * cos(theta) - gh * sin(theta))), radius * cos(t) * sin(theta) + gh * cos(theta));
+}
+//レンズ配置パス関数　ローカル　これに沿ってスキャンライン近辺のレンズが並んでいる
+ureal NodeLensesPath(ureal t,ureal gh,ureal theta) {
+	return sqrt(3) / 2. * gh;
+}
+//レンズ配置パス関数　ローカル　これに沿ってレンズを並べると理想通りのスキャン軌跡の高さとなる
+ureal NodeLensesPathCross(ureal t, ureal gh, ureal theta) {
+	return sqrt(1 - pow(gh,2)) * cos(t) * sin(theta) - (2 * sqrt(1 - pow(gh,2)) * t * sin(theta)) / std::numbers::pi + gh * cos(theta);
+}
+//レンズパスからのスキャン軌跡の高さ レンズパスを指定してね
+ureal ScanHeightFromLensesPath(ureal t, ureal gh, ureal theta, const std::function<ureal(ureal, ureal, ureal)>& lensespath) {
+	return ScanPointWithGh(t, gh, theta)[1] - lensespath(t, gh, theta);
 }
 
 int main() {
@@ -23,8 +35,8 @@ int main() {
 		DefinePythonFunctions(plotter);//梅本的基本関数を定義
 
 		//高さを変えてplt
-		constexpr size_t scanheightResolution = 10;
-		for (std::decay<decltype(scanheightResolution)>::type h = 0; h < scanheightResolution; h++) {
+		constexpr size_t scanheightResolution = 40;
+		while(1)for (std::decay<decltype(scanheightResolution)>::type h = 0; h < scanheightResolution; h++) {
 			plotter->send_command("plt.clf()\n");
 			plotter->send_command("t=[]\nv=[]\nr=[]\n");
 
@@ -37,8 +49,10 @@ int main() {
 				const ureal t = uleap(std::make_pair(-std::numbers::pi, +std::numbers::pi), phi / (ureal)circleRes);//tを計算
 
 				const auto scanUV = ScanPointWithGh(t, scanHeight, theta);//UV座標ゲット UV球ローカル
+				const auto scanHFromLensesPath = ScanHeightFromLensesPath(t, scanHeight, theta, NodeLensesPathCross);//レンズパスからの高さ UV球ローカル
+				//...これを直線にしなきゃいけない　
 
-				plotter->send_command(StringFormat("t.append(%f)\nv.append(%f)\nr.append(%f)\n", t, scanUV.y(), scanUV.x()));
+				plotter->send_command(StringFormat("t.append(%f)\nv.append(%f)\nr.append(%f)\n", t, scanHFromLensesPath, scanUV.x()));
 			}
 
 			plotter->send_command(StringFormat("plt.text(0,-1.5,\"theta = 30[deg], scan height = %f\")",scanHeight));
