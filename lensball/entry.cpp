c38509fbe83134f6769cfe7c74642af31ca9097d
遼太郎 梅本 <lissyno_original@outlook.jp>
2023-06-18 22:55:14 +0900

    行方向の当たり判定を実行

--- lensball/entry.cpp
+++ lensball/entry.cpp
@@ -142,11 +142,13 @@ mlab.mesh(%f*spx, %f*spy, %f*spz ,color=(1.,1.,1.) )
 		const ureal rowLength = 2. * pi * cos(rowAngle);//行の長さ
 		const ureal lensEdgeWidth = rowLength / (ureal)lensNumInCollum / 2.;
 		const ureal eachRowsDistance = 1.5 * rowLength / sqrt(3.) / (ureal)lensNumInCollum;//六角形の一変だけシフトする
-		const Eigen::Rotation2D<ureal> localPlaneToGrobal(rowAngle);
+		const auto DesignedMapToMap = bitrans<Eigen::Rotation2D<ureal>>(Eigen::Rotation2D<ureal>(rowAngle));//レンズアレイを傾斜させる前から傾斜させたあとにする
 
 		const ureal nodeLensRadius = 2. * lensEdgeWidth / sqrt(3.);//要素レンズ形状を作成　球の直径
-		const std::pair<size_t,size_t> nodeLensResolution = make_pair(5*2,7);//要素レンズの分割数
+		const std::pair<size_t,size_t> nodeLensResolution = make_pair(5*2,5);//要素レンズの分割数
 		constexpr size_t rowNum = 15;//奇数にしてね
+		
+		//std::list<uleap>//マップ座標でのレンズ中心
 		for (std::decay<decltype(rowNum)>::type rd = 0; rd < rowNum; rd++) {
 			const ureal tlati = eachRowsDistance * rd-(eachRowsDistance*(ureal)(rowNum-1)/2.);//lati方向の現在位置
 			const bool eachFlag = rd % 2;//交互に切り替わるフラグ
@@ -175,7 +177,7 @@ mlab.mesh(%f*spx, %f*spy, %f*spz ,color=(1.,1.,1.) )
 							uleap(PairMinusPlus(pi / 2.), nlla / (ureal)(nodeLensResolution.second - 1)));//要素レンズローカルでの極座標
 
 						const uvec3 nodelensShape = nodeLensRadius * PolarToXyz(localpos);//これが円になるはず
-						const uvec2 nodelensGrobalMap = localPlaneToGrobal * (uvec2(nodelensShape.x(), nodelensShape.y()) + localcenter);//マップローカルでの要素レンズ
+						const uvec2 nodelensGrobalMap = (DesignedMapToMap.prograte()) * (uvec2(nodelensShape.x(), nodelensShape.y()) + localcenter);//マップローカルでの要素レンズ
 
 
 						const auto polarpos = MapToPolar(nodelensGrobalMap);//つぎにローカル極座標を得る
@@ -190,12 +192,12 @@ mlab.mesh(%f*spx, %f*spy, %f*spz ,color=(1.,1.,1.) )
 				//頂点を転送して描
 				hexvertices.push_back(hexvertices.front());//一周するために最初の点を末尾に挿入
 				ResetPyVecSeries(mlabSeries);
-				for (auto v : hexvertices) {
-					const auto localHeight=sqrt(1.-pow(v.norm()/nodeLensRadius,2.))*nodeLensRadius;//ローカル座標で高さを決める
-					v = localPlaneToGrobal * (v + localcenter);
-					AppendPyVecSeries(pypltSeries, v);
-					const auto polarpos = MapToPolar(v);//つぎに極座標を得る
-					AppendPyVecSeries(mlabSeries, Polar3DToXyz(uvec3(polarpos.x(), polarpos.y(), sphereRadius + localHeight)));
+				for (const auto& v : hexvertices) {
+					const uvec2 designedVertex = (v + localcenter);
+					const uvec2 vertex = DesignedMapToMap.prograte() * designedVertex;//傾けてマップ座標にする
+					AppendPyVecSeries(pypltSeries, designedVertex);
+					const auto polarpos = MapToPolar(vertex);//つぎに極座標を得る
+					AppendPyVecSeries(mlabSeries, PolarToXyz(polarpos));
 				}
 
 				py::sf("plt.plot(%s,color=(0,0,0))", GetPySeriesForPlot(pypltSeries));
@@ -205,16 +207,41 @@ mlab.mesh(%f*spx, %f*spy, %f*spz ,color=(1.,1.,1.) )
 
 
 		//スキャンをする
-		constexpr size_t projectorResInTheta = 40;//プロジェクタの縦がわ解像度
+		constexpr size_t projectorResInTheta = 2;//プロジェクタの縦がわ解像度
 		constexpr ureal projectorHalfAngle = 60. / 180. * pi;//プロジェクトの投映角
-		constexpr size_t numOfProjectionPerACycle = 720;//一回転での投影数
+		constexpr size_t numOfProjectionPerACycle = 2;//一回転での投影数
 		for (std::decay<decltype(numOfProjectionPerACycle)>::type rd = 0; rd < numOfProjectionPerACycle; rd++) {
 			const ureal ballRotation = uleap(make_pair(0., 2. * pi), rd / (ureal)(numOfProjectionPerACycle));//ボールの回転角度
 			for (std::decay<decltype(projectorResInTheta)>::type pd = 0; pd < projectorResInTheta; pd++) {//プロジェクタの注目画素
 				const ureal rayThetaInProjectorLocal = uleap(PairMinusPlus(projectorHalfAngle), pd / (ureal)(projectorResInTheta - 1));//プロジェクタ座標系での注目画素からでるレイのθ
 
 				const uvec2 rayDirInBallLocalPolar(-ballRotation, rayThetaInProjectorLocal);//ボールの回転角度画からボールローカルでのレイの方向(極座標がわかる)
-				//もちろん交点もここ
+				const uvec2 rayDirInBallLocalMap = PolarToMap(rayDirInBallLocalPolar);//マップ座標はここ 傾いたあと
+				const uvec2 rayDirInBallLocalMapDesigned = (DesignedMapToMap.untiprograte()) * rayDirInBallLocalMap;//傾ける前 デザインマップ
+				py::sf("plt.scatter(%f,%f,color=(0,%f,%f))", rayDirInBallLocalMapDesigned.x(), rayDirInBallLocalMapDesigned.y(),rd/1.,pd/1.);
+
+				//デザインマップのシータからrowがわかる
+				//const ureal tlati = eachRowsDistance * rd-(eachRowsDistance*(ureal)(rowNum-1)/2.);//lati方向の現在位置
+				//シータは-eachRowsDistance*(ureal)(rowNum-1)/2~eachRowDistance*(rownum-1)/2まで
+				//まずここに正規化する 一番下のrowの高さちょうどのとき 0　一番上のrowの高さちょうどのときrowNum-1になるようにする
+				const ureal regRayDirLati=[&] {
+					const ureal zerothRowlati = -(eachRowsDistance * (ureal)(rowNum - 1) / 2.);//zero番目の行の高さ
+					const ureal zeroSetRayDir = rayDirInBallLocalMapDesigned.y() - zerothRowlati;//0番目の行の高さに始点を合わせたレイの高さ
+					const ureal finalRowlati = (eachRowsDistance * (ureal)(rowNum - 1) / 2.);//rowNum-1番目の行の高さ
+					//スケーリング　fin~0までのスケールがrowNum-1~0までのスケールになって欲しい
+					const ureal thisscale = (ureal)(rowNum - 1 - 0) / (finalRowlati - zerothRowlati);
+				
+					return thisscale * zeroSetRayDir;
+				}();
+
+				//四捨五入するともっともらしいインデックスがわかる
+				const int centerRawIndex = round(regRayDirLati);
+				//隣り合う行のもっともらしいインデックスもわかる
+				const int neibourRawIndex = (regRayDirLati - (ureal)centerRawIndex) > 0. ? centerRawIndex + 1 : centerRawIndex - 1;
+				printf("%d,%d\n", centerRawIndex, neibourRawIndex);
+				// 
+				// 
+				//const ureal tlonn = uleap(PairMinusPlus(rowLength/2.), ld / (ureal)lensNumInCollum) + (eachFlag ? ((rowLength) / (ureal)lensNumInCollum / 2.) : 0.);
 			}
 		}
 		
